a1be07d27a9eb5a67f942b3451e724c9cdd05de8
ARedondoVilla <alexredondovilla@gmail.com>
2020-10-23 16:34:39 +0000

    mentoria

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 // LOS COMPONENETES SON FUNCIONES QUE RECIBEN COMO PARAMETROS LAS PROPIEDADES props (QUE VIENEN DEL main/index.js)
 // ESTOS COMPONENTES CONTIENEN EL CONTENIDO DEL html
+
 export function Home(props) {
 	const [counter, setCounter] = useState(0);
 
-	useEffect(() => {
-		counter + 1;
-	});
+	useEffect(
+		() => {
+			console.log("Entro");
+			return function() {
+				// FUNCION DE CIERRE
+				console.log("Salgo");
+			};
+		},
+		[counter]
+	);
 
 	return (
 		<main className="page">
